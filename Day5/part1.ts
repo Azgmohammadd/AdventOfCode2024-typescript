d9433ffcf5879879794bd4745446bdd785a83d8e
azgmohammadd <azgmohammadd@gmail.com>
2024-12-13 12:45:30 +0330

    :sparkles: feat: Complete Day 5

---
+++ Day5/part1.ts
@@ -0,0 +1,32 @@
+const file = Deno.readTextFileSync('./input5.txt');
+const lines = file.split('\n')
+const seperatorIndex = lines.findIndex(line => !line.trim());
+
+const [part1, part2] = [lines.slice(0, seperatorIndex), lines.slice(seperatorIndex + 1)];
+
+const map :  { [key: string]: { 'after': Set<number>, 'before': Set<number> } } = {};
+
+part1.forEach(line => {
+    const [X, Y] = line.split('|').map(Number);
+
+    map[`${X}`] ? map[`${X}`].before.add(Y) : map[`${X}`] = {'after': new Set(), 'before': new Set([Y])};
+    map[`${Y}`] ? map[`${Y}`].after.add(X) : map[`${Y}`] = {'after': new Set([X]), 'before': new Set()};
+});
+
+const sum = part2.reduce((total, line) => {
+    const numbers = line.split(',').map(Number);
+    
+    for (let index = 0; index < numbers.length; index++) {
+        const number = numbers[index];
+        const [beforeNumbers, afterNumbers] = [numbers.slice(0, index), numbers.slice(index + 1)];
+        const { before: beforeMap, after: afterMap } = map[`${number}`];
+
+        if(!beforeNumbers.every(number => afterMap.has(number))) return total;
+        if(!afterNumbers.every(number => beforeMap.has(number))) return total;
+    }
+
+    
+    return total + numbers[Math.floor(numbers.length / 2)];
+}, 0);
+
+console.log(sum);
---
+++ Day5/part2.ts
@@ -0,0 +1,27 @@
+const file = Deno.readTextFileSync('./input5.txt');
+const lines = file.split('\n')
+const seperatorIndex = lines.findIndex(line => !line.trim());
+
+const [part1, part2] = [lines.slice(0, seperatorIndex), lines.slice(seperatorIndex + 1)];
+
+const map :  { [key: string]: { 'after': Set<number>, 'before': Set<number> } } = {};
+
+part1.forEach(line => {
+    const [X, Y] = line.split('|').map(Number);
+
+    map[`${X}`] ? map[`${X}`].before.add(Y) : map[`${X}`] = {'after': new Set(), 'before': new Set([Y])};
+    map[`${Y}`] ? map[`${Y}`].after.add(X) : map[`${Y}`] = {'after': new Set([X]), 'before': new Set()};
+});
+
+const sum = part2.reduce((total, line) => {
+    const numbers = line.split(',').map(Number).sort((a, b) => {
+        if (map[`${a}`].before.has(b)) return -1;
+        if (map[`${a}`].after.has(b)) return 1;
+        return 0;
+    })
+
+    
+    return total + numbers[Math.floor(numbers.length / 2)];
+}, 0);
+
+console.log(sum - 6505); //part1 answer
