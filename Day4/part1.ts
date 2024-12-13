8ed9fcc002e3a9577bfab45f829166295a5baffa
azgmohammadd <azgmohammadd@gmail.com>
2024-12-13 12:45:20 +0330

    :sparkles: feat: Complete Day 4

---
+++ Day4/part1.ts
@@ -0,0 +1,76 @@
+const file = await Deno.readTextFile('./input4.txt')
+
+const lines = file.split('\n')
+let sum = 0;
+
+function horizontal(lineIndex: number, charIndex: number): void {
+    //Check XMAS
+    if (lines[lineIndex].length - 1 >= charIndex + 3)
+        if (lines[lineIndex].slice(charIndex, charIndex+4).includes('XMAS')) sum += 1;
+    
+    //Check SAMX
+    if (0 <= charIndex - 3)
+        if (lines[lineIndex].slice(charIndex-3, charIndex + 1).includes('SAMX')) sum += 1;
+}
+
+function vertical(lineIndex: number, charIndex: number) : void {
+    /* chack
+        X
+        M
+        A
+        S
+    */
+    if (lines.length - 1 >= lineIndex + 3) {
+        if (lines[lineIndex + 1][charIndex] === 'M' &&  lines[lineIndex + 2][charIndex] === 'A' && lines[lineIndex + 3][charIndex] === 'S') sum += 1;
+    }
+
+    /* chack
+        S
+        A
+        M
+        X
+    */
+   if (lineIndex >= 3) {
+        if (lines[lineIndex - 1][charIndex] === 'M' && lines[lineIndex - 2][charIndex] === 'A' && lines[lineIndex - 3][charIndex] === 'S') sum += 1;
+    }
+
+}
+
+function diagonal(lineIndex: number, charIndex: number) : void {
+    if (lineIndex + 3 <= lines.length - 1) {
+        if (lines[lineIndex].length - 1 >= charIndex + 3) {
+            if (lines[lineIndex + 1][charIndex + 1] === 'M' && lines[lineIndex + 2][charIndex + 2] === 'A' && lines[lineIndex + 3][charIndex + 3] === 'S') sum += 1;
+        }
+    }
+
+    if (lineIndex >= 3) {
+        if (charIndex >= 3) {
+            if (lines[lineIndex - 1][charIndex - 1] === 'M' && lines[lineIndex - 2][charIndex - 2] === 'A' && lines[lineIndex - 3][charIndex - 3] === 'S') sum += 1;
+        }
+    }
+    
+    if (lineIndex + 3 <= lines.length - 1) {
+        if (charIndex - 3 >= 0) {
+            if (lines[lineIndex + 1][charIndex - 1] === 'M' && lines[lineIndex + 2][charIndex - 2] === 'A' && lines[lineIndex + 3][charIndex - 3] === 'S') sum += 1;
+        }
+    }
+
+    if (lineIndex >= 3) {
+        if (lines[lineIndex].length - 1 >= charIndex + 3) {
+            if (lines[lineIndex - 1][charIndex + 1] === 'M' && lines[lineIndex - 2][charIndex + 2] === 'A' && lines[lineIndex - 3][charIndex + 3] === 'S') sum += 1;
+        }
+    }
+}
+
+for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+    for (let charIndex = 0; charIndex < lines[lineIndex].length; charIndex++) {
+        if (lines[lineIndex][charIndex] === 'X') {
+            horizontal(lineIndex, charIndex);
+            vertical(lineIndex, charIndex);
+            diagonal(lineIndex, charIndex);
+        }
+    }
+}
+
+console.log(sum);
+
---
+++ Day4/part2.ts
@@ -0,0 +1,45 @@
+const file = Deno.readTextFileSync('./input4.txt');
+const lines = file.split('\n');
+let sum = 0;
+
+for (let lineIndex = 0; lineIndex < lines.length - 2; lineIndex++) {
+    for (let charIndex = 0; charIndex < lines[lineIndex].length - 2; charIndex++) {
+        if (
+            lines[lineIndex][charIndex] === 'M' &&
+            lines[lineIndex + 2][charIndex] === 'M' &&
+            lines[lineIndex][charIndex + 2] === 'S' &&
+            lines[lineIndex + 2][charIndex + 2] === 'S' &&
+            lines[lineIndex + 1][charIndex + 1] === 'A'
+        )
+        sum += 1;
+        
+        if (
+            lines[lineIndex][charIndex] === 'S' &&
+            lines[lineIndex + 2][charIndex] === 'S' &&
+            lines[lineIndex][charIndex + 2] === 'M' &&
+            lines[lineIndex + 2][charIndex + 2] === 'M' &&
+            lines[lineIndex + 1][charIndex + 1] === 'A'
+        )
+        sum += 1;
+
+        if (
+            lines[lineIndex][charIndex] === 'S' &&
+            lines[lineIndex + 2][charIndex] === 'M' &&
+            lines[lineIndex][charIndex + 2] === 'S' &&
+            lines[lineIndex + 2][charIndex + 2] === 'M' &&
+            lines[lineIndex + 1][charIndex + 1] === 'A'
+        )
+        sum += 1;
+
+        if (
+            lines[lineIndex][charIndex] === 'M' &&
+            lines[lineIndex + 2][charIndex] === 'S' &&
+            lines[lineIndex][charIndex + 2] === 'M' &&
+            lines[lineIndex + 2][charIndex + 2] === 'S' &&
+            lines[lineIndex + 1][charIndex + 1] === 'A'
+        )
+        sum += 1;
+    }    
+}
+
+console.log(sum);
