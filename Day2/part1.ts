e015e4d8bcad11caba67dee487b4889d29cef7e2
azgmohammadd <azgmohammadd@gmail.com>
2024-12-13 12:43:14 +0330

    onst file = Deno.readTextFileSync('./input5.txt');

---
+++ Day2/part1.ts
@@ -0,0 +1,27 @@
+const file = await Deno.readTextFile('./input2.txt');
+
+const reports = file.split('\n').map(line => line.split(' ').map(Number));
+
+const sum = reports.reduce((total: number, report: number[]) => {
+  const strictlyIncreasing = report[1] > report[0];
+
+  for (let index = 1; index < report.length; index++) {
+    const diff = report[index] - report[index - 1];
+  
+    if (diff === 0) return total;
+    
+    if (strictlyIncreasing) {
+      if (!(1 <= diff && diff <= 3)) {
+        return total;
+      }
+    }
+    else {
+      if(!(-3 <= diff && diff <= -1)) {
+        return total;
+      }
+    }
+  }
+  return total + 1;
+}, 0);
+
+console.log(sum);
